92305d61ac2aa813d2a2e281a1dc8853ebdb0665
Ashpan Raskar <Ashpanr@gmail.com>
2020-03-16 21:58:25 -0400

    fixed heapsort, added testclass

--- Lab5/src/MaxHeap.java
+++ Lab5/src/MaxHeap.java
@@ -2,16 +2,16 @@ import java.util.Arrays;
 
 public class MaxHeap {
     private Integer[] heap;
-    int maxSize; //2^n should be size
-    int currSize;
+    private int maxSize;
+    private int currSize;
 
-    public MaxHeap(int treeSize){
+    public MaxHeap(int treeSize){  //init heap with 0 elements and an array of null values
         maxSize = treeSize;
         currSize = 0;
         heap = new Integer[treeSize];
     }
 
-    public MaxHeap(Integer[] someArray){
+    public MaxHeap(Integer[] someArray){ //initialize heap with each element from someArray using insert method
         maxSize = someArray.length;
         currSize = 0;
         heap = new Integer[maxSize];
@@ -21,72 +21,58 @@ public class MaxHeap {
     }
 
     public void insert(int n){
-        if(maxSize == currSize)
+        if(maxSize == currSize) //if it will exceed current size, resize the array and reinsert the elements
             Reheap();
         boolean empty = false;
         int i = currSize;
-        heap[i] = n;
+        heap[i] = n; //insert new element into last empty spot in array
         boolean noSwap = false;
-        while(!noSwap) { //swap until no longer needed
-            int parentIndex = (i - 1) / 2;
-            if (heap[parentIndex] < n) { //flip with parent
+        while(!noSwap) { //swap until children are smaller than parent
+            int parentIndex = (i - 1) / 2; //value of parent index
+            if (heap[parentIndex] < n) { //if not a heap, flip with parent
                 int temp = heap[parentIndex];
                 heap[parentIndex] = n;
                 heap[i] = temp;
                 i = parentIndex;
-            }else{
+            }else{ //is a heap so set bool to true to end while loop
                 noSwap = true;
             }
         }
-        currSize++;
+        currSize++; //increment sizecounter
     }
-    private boolean isHeap(int index){
-        int left = index*2+1;
-        int right = index*2+2;
-        if(left>=heap.length){
-            return true;
-        }
-        return((heap[left] == null || heap[index] >= heap[left]) && (heap[right] == null || heap[index] >= heap[right]));
-    }
-
 
     private int deleteMax(){
-        int max = heap[0];
+        int max = heap[0]; //delete max elem and replace with last added elem
         heap[0] = heap[currSize-1];
         heap[currSize-1] = null;
         int current = 0;
-        while(!isHeap(current)){
-            int left = current*2+1;
+        while(!isHeap(current)){ //while current index isnot true for a heap
+            int left = current*2+1; //left and right indices
             int right = current*2+2;
-            if(heap[left] != null){
-                if(heap[right] == null){
+            if(heap[left] != null){ //if left child is not null
+                if(heap[right] == null){ //but right is, flip right left child and parent
                     int temp = heap[left];
                     heap[left] = heap[current];
                     heap[current] = temp;
                     current = left;
-                }else {
+                }else { //if right child is not null
                     int index = 0;
-                    if(heap[left] >= heap[right])
+                    if(heap[left] >= heap[right]) //if left child > right child, swap parent and left
                         index = left;
-                    else
+                    else //if right child > left child, swap parent and right
                         index = right;
                     int temp = heap[index];
                     heap[index] = heap[current];
                     heap[current] = temp;
                     current = index;
                 }
-            }else
+            }else //if left child is null, end while loop, nothing can be done anymore
                 break;
         }
         currSize--;
         return max;
     }
-    public static void heapsort(Integer[] arrayToSort){
-        MaxHeap sorted = new MaxHeap(arrayToSort);
-        for (int i = sorted.currSize-1; i >= 0; i--) {
-            arrayToSort[i] = sorted.deleteMax();
-        }
-    }
+
     public String toString(){
         String output = "";
         for(int i=0; i<currSize; i++) {
@@ -95,24 +81,38 @@ public class MaxHeap {
             else
                 output += (heap[i] + ", ");
         }
-    return output;
+        return output;
     }
 
+    public static void heapsort(Integer[] arrayToSort){
+        MaxHeap sorted = new MaxHeap(arrayToSort); //create heap of array
+        int maxLoop = sorted.currSize; //number of times loop should run
+        for (int i = 0; i < maxLoop; i++) {
+            arrayToSort[i] = sorted.deleteMax(); //add value from delete max to array until heap is completed
+        }
+    }
 
-    public int getCurrSize(){
+    public int getSize(){
         return currSize;
     }
-
-    public int getMaxSize(){
+    public int getCapacity(){
         return maxSize;
     }
-    private void Reheap(){
+
+    private void Reheap(){ //resize heap
         Integer[] newHeap = new Integer[2*maxSize];
         maxSize *= 2;
         for (int i = 0; i < heap.length; i++) {
             newHeap[i] = heap[i];
         }
         heap = newHeap;
+    }
+    private boolean isHeap(int index){ //checks if value at index follows max heap conditions
+        int left = index*2+1;
+        int right = index*2+2;
+        if(left>=heap.length){ return true; }
+        if(right>=heap.length){ return true; }
+        return((heap[left] == null || heap[index] >= heap[left]) && (heap[right] == null || heap[index] >= heap[right]));
     }
 
     public static void main(String[] args) {
@@ -125,11 +125,11 @@ public class MaxHeap {
         heap.insert(5);
         System.out.println(heap.toString());
         System.out.println(heap.deleteMax());
+        System.out.println(heap.deleteMax());
         System.out.println(heap.toString());
-        Integer[] test = new Integer[] {5,345,5,543,634,5,334,2,34,1234,74};
+        Integer[] test = new Integer[] {-5,345,5,-543,-634,5,334,2,34,1234,74};
         System.out.println(Arrays.toString(test));
-        MaxHeap newHeap = new MaxHeap(test);
-        System.out.println(newHeap.toString());
+        System.out.println("Heap: \t" + new MaxHeap(test).toString());
         heapsort(test);
         System.out.println(Arrays.toString(test));
 
